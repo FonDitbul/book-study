908321d904785df752c08dacc6d0a0c8b132a871
FonDitbul <niki7084@naver.com>
2022-02-18 17:07:22 +0900

    7장 나이브 베이즈 분류 리팩토링 #5 Mocha

--- 리팩토링 자바스크립트/ch07/nb_refactoring.js
+++ 리팩토링 자바스크립트/ch07/nb_refactoring.js
@@ -114,3 +114,10 @@ function classify(chords) {
 
 classify(["d", "g", "e", "dm"]);
 classify(["f#m7", "a", "dadd9", "dmaj7", "bm", "bm7", "d", "f#m"]);
+
+var wish = require("wish");
+describe("the file", function () {
+  it("works", function () {
+    wish(true);
+  });
+});
